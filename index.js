c241e8d8b5177aa7a00a248cb92971f77342b3c9
Augusto <tristedameianoite@gmail.com>
2026-01-06 08:58:15 -0300

    did the server side of /post

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/",routes);
-
+app.use("/form", routes);
+app.use("/post", routes);
 
 
 app.listen(port, ()=>{
--- routes.js
+++ routes.js
@@ -3,9 +3,32 @@ import { Router } from "express";
 
 
 const router = Router();
+let userIdIncrement = 1;
+let hist = [];
 
 router.get("/", (req, res) =>{
     res.render("index");
+})
+router.get("/form", (req, res) => {
+     res.render("form")
+})
+
+router.post("/post", async (req, res) => {
+try{
+  const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
+  title: req.body.title,
+  body: req.body.text,
+  userId: userIdIncrement
+})
+const result = response.data;
+hist.push(result);
+userIdIncrement++;
+
+res.render("form",{result});
+  } 
+catch(error){
+    res.status(500).json({ error: "Error to create post" });
+}
 })
 
 
