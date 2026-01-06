2fc64e4d9b327b81805ca9c08c29de281beb6ec0
Augusto <tristedameianoite@gmail.com>
2026-01-06 12:47:43 -0300

    Backend finished

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ let userIdIncrement = 1;
 let hist = [];
 
 router.get("/", (req, res) =>{
-    res.render("index");
+    res.render("index",{hist});
 })
 router.get("/form", (req, res) => {
      res.render("form")
@@ -24,12 +24,46 @@ const result = response.data;
 hist.push(result);
 userIdIncrement++;
 
-res.render("form",{result});
+res.render("form",{result,hist});
   } 
 catch(error){
     res.status(500).json({ error: "Error to create post" });
 }
 })
+
+router.post("/delete/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const index = hist.findIndex(post => post.id === id);
+  if (index !== -1) {
+    hist.splice(index, 1);
+  }
+  return res.redirect("/");
+});
+
+router.get("/edit/:id", (req, res) =>{
+    const id = Number(req.params.id);
+    const post = hist.find(p => p.id === id);
+    if(!post){
+        return res.redirect("/");
+    }
+    res.render("edit", { post});
+})
+
+router.post("/edit/:id", async (req, res) =>{
+const id = Number(id.params.id);
+const post = hist.find(p => p.id === id);
+
+if (!post) {
+    return res.redirect("/")
+};
+
+post.title = req.body.title;
+post.body = req.body.body;
+
+res.redirect("/");
+
+})
+
 
 
 
